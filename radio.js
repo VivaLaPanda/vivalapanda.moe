bb66d5da26fa22b38f697d31e4adf1bb0a9a7b73
Adrian Smith (VivaLaPanda) <adrian@smithdev.io>
2021-04-15 21:03:57 -0700

    adding viz

--- radio.js
+++ radio.js
@@ -5,6 +5,7 @@ window.onload = function(){
     setInterval(function() {
         getPlaylistInfo();
     }, 60 * 1000); // 60 * 1000 milsec
+	
 }
 
 /**
@@ -57,7 +58,9 @@ function getPlaylistInfo(){
 
 function setup(){
     var audio = $('#audioPlayer')[0];
-    var src = 'https://VivaLaPanda.moe/stream.mp3';
+	// TODO PLACEHOLDER AUDIO
+    // var src = 'https://VivaLaPanda.moe/stream.mp3';
+    var src = '/audio/mercurius_pretty-the_birth_of_homunculus.mp3';
     audio.volume = 1;
 
     function preload(imgurl) {
@@ -100,19 +103,20 @@ function setup(){
             //if (navigator.userAgent.match(/Firefox/)) {
             var data = $(audio).data('src');
             audio.src = data ? data : (src + '?nocache=' + Date.now());
-            //}
-            //else
-            //{
-            //    audio.src = src;
-            //}
+			
             audio.load();
             audio.play();
+			
+			window.setTimeout(()=>{
+				startVisualizer();
+			}, 1000);
             // ga('send', 'event', 'Stream', 'play', 'Stream play tracking');
         });
         $('#audioStop').click(function () {
             audio.pause();
             // ga('send', 'event', 'Stream', 'stop', 'Stream play tracking');
             audio.src = '';
+			stopVisualizer();
         });
         $('#audioMute').click(function () {
             audio.muted = !audio.muted;
@@ -127,3 +131,120 @@ function setup(){
     }
     $.bindAudioControls();
 };
+
+var visualizerStopped = true;
+var firstClick = true;
+function startVisualizer() {
+	stream = document.getElementById("audioPlayer").captureStream();
+	
+    //main block for doing the audio recording
+    let chunks = [];
+    const mediaRecorder = new MediaRecorder(stream);
+
+    // visualiser setup - create web audio api context and canvas
+
+    let audioCtx;
+    const visCanvas = document.querySelector('#visualizer');
+    const visCanvasCtx = visCanvas.getContext("2d");
+	
+	if (firstClick) {
+		visCanvasCtx.scale(8,4);
+		firstClick = false;
+	}
+	
+    visualizerStopped = false;
+
+    // Do the visualizer stuff
+    visualize(stream);
+    mediaRecorder.start();
+    console.log(mediaRecorder.state);
+    console.log("recorder started");
+
+    mediaRecorder.ondataavailable = function(e) {
+        chunks.push(e.data);
+    }
+
+    function visualize() {
+        if(!audioCtx) {
+            audioCtx = new AudioContext();
+        }
+    
+        const source = audioCtx.createMediaStreamSource(stream);
+    
+        const analyser = audioCtx.createAnalyser();
+        analyser.fftSize = 32;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+    
+        source.connect(analyser);
+        //analyser.connect(audioCtx.destination);
+    
+        let index = 0;
+        draw()
+    
+        function draw() {
+            index++;
+            if (visualizerStopped) {
+                return;
+            }
+
+            const WIDTH = visCanvas.width / 8;
+            const HEIGHT = visCanvas.height / 4;
+			const barHeight = 8;
+        
+            requestAnimationFrame(draw);
+			
+			if (index % 4 == 0) {
+				analyser.getByteTimeDomainData(dataArray);
+			}
+			
+            visCanvasCtx.lineWidth = 1;
+        
+        
+            let sliceWidth = WIDTH * 1.0 / bufferLength;
+            let x = 0;
+        
+        
+            for(let i = 0; i < bufferLength; i++) {
+        
+                let v = dataArray[i] / 128.0;
+                let y = (v - .7) * HEIGHT;
+        
+				// 284 h, half empty so 142. 32 bars
+				// // visCanvasCtx.lineTo(x, y);
+				visCanvasCtx.beginPath();
+				for (let barIdx = 0; ((barIdx * 2) * barHeight) < HEIGHT; barIdx++) {
+					let barY = (barIdx * 2) * barHeight;
+					visCanvasCtx.rect(x + .5, HEIGHT - barY, sliceWidth - 1, barHeight);
+				}
+				visCanvasCtx.fillStyle = "rgba(0,0,0,.1)";
+				visCanvasCtx.fill();
+				
+				for (let barIdx = 0; ((barIdx * 2) * barHeight) < y; barIdx++) {
+					visCanvasCtx.beginPath();
+					let barY = (barIdx * 2) * barHeight;
+					visCanvasCtx.rect(x + .5, HEIGHT - barY, sliceWidth - 1, barHeight);
+					
+					if (barIdx < 8) {
+						visCanvasCtx.fillStyle = "#ffee55";
+					} else if (barIdx < 9) {
+						visCanvasCtx.fillStyle = "#ff0066";
+					} else {
+						visCanvasCtx.fillStyle = "#00eebb";
+					}
+					
+					visCanvasCtx.fill();
+				}
+        
+                x += sliceWidth;
+            }
+        }
+    }
+}
+
+function stopVisualizer() {
+    visualizerStopped = true;
+    const visCanvas = document.querySelector('#visualizer');
+    const visCanvasCtx = visCanvas.getContext("2d");
+    visCanvasCtx.clearRect(0,0, visCanvas.width, visCanvas.height)
+}
