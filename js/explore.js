f612cc78fed82d0825ccaf23162e357fb8ca9c09
Adrian Smith (VivaLaPanda) <adrian@smithdev.io>
2021-08-28 14:41:16 -0700

    Built out a few rooms

--- js/explore.js
+++ js/explore.js
@@ -23,3 +23,18 @@ function toggleMute() {
 function goTo(location) {
 	window.location.href = location + ".html";
 }
+
+var haveTalkedToBarshoujo = false;
+function talkToBarshoujo(event) {
+	if (haveTalkedToBarshoujo) {
+		haveTalkedToBarshoujo = false;
+		document.getElementById("explore-img").setAttribute("style", "");
+		document.getElementById("towerbar-text").innerHTML = "\"Come over here friend, grab yourself a seat!\""
+		document.getElementById("towerbar-speaker").innerHTML = "T o n y"
+	} else {
+		haveTalkedToBarshoujo = true;
+		document.getElementById("explore-img").setAttribute("style", "background-image: url('/img/explore/portraits/bar_lady.png');background-position: center;");
+		document.getElementById("towerbar-text").innerHTML = "\"Hey cutie, haven't seen you here before. Come share a drink with me\""
+		document.getElementById("towerbar-speaker").innerHTML = "B a r s h o u j o"
+	}
+}
